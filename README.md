37c5ad303dbc8ff72bf96fd357cf9536c0c38f8d
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-24 16:41:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#
+
+<div align="center">
+  
+# ✔ HTML, CSS and JavaScript: To-Do List
+
+</div>
+
+Este proyecto es una **Lista de Tareas** hecha con **HTML**, **CSS** y **JavaScript** de manera totalmente vanilla.
+
+🧩 Aquí puedes ver su [**Live Demo.**](https://to-do-list-abraham.netlify.app/)
+
+Este proyecto contiene:
+
+- Manejo del **DOM** con JavaScript.
+- **EventListener**.
+- **Persistencia** de datos con el **LocalStorage.**
---
+++ app.js
@@ -0,0 +1,251 @@
+const toggleBtn = document.querySelector(".btn-theme");
+
+toggleBtn.addEventListener("click", () => {
+  // toggle the dark-theme class on the button
+  document.documentElement.classList.toggle("dark-theme");
+
+  // save the current theme to local storage
+  localStorage.setItem("theme", document.documentElement.classList.contains("dark-theme") ? "dark" : "light");
+
+  toggleBtn.innerHTML = document.documentElement.classList.contains("dark-theme") ? `<i class="fas fa-sun"></i>` : `<i class="fas fa-moon"></i>`;
+});
+
+// check if a theme is saved in local storage
+const savedTheme = localStorage.getItem("theme");
+
+// if a theme is saved, set it as the current theme
+if (savedTheme === "dark") {
+  document.documentElement.classList.add("dark-theme");
+}
+
+// ****** SELECT ITEMS **********
+const form = document.querySelector(".todo-form");
+const alert = document.querySelector(".alert");
+const todo = document.getElementById("todo");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".todo-container");
+const list = document.querySelector(".todo-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ****** EVENT LISTENERS **********
+
+// submit form
+form.addEventListener("submit", addItem);
+
+// clear list
+clearBtn.addEventListener("click", clearItems);
+
+// display items onload
+window.addEventListener("DOMContentLoaded", setupItems);
+
+// ****** FUNCTIONS **********
+
+// add item
+function addItem(e) {
+  e.preventDefault();
+  const value = todo.value;
+  const id = new Date().getTime().toString();
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("todo-item");
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-pen"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-times"></i>
+              </button>
+            </div>
+          `;
+    // add event listeners to both buttons;
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editItem);
+
+    // append child
+    list.appendChild(element);
+    // display alert
+    displayAlert("✅ item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // set local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
+  } else if (value !== "" && editFlag) {
+    editElement.innerHTML = value;
+    displayAlert("value changed", "success");
+
+    // edit  local storage
+    editLocalStorage(editID, value);
+    setBackToDefault();
+  } else {
+    displayAlert("🚨 please enter value", "danger");
+  }
+}
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  // remove alert
+  setTimeout(() => {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+// clear items
+function clearItems() {
+  const items = document.querySelectorAll(".todo-item");
+  if (items.length > 0) {
+    items.forEach((item) => {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger");
+  setBackToDefault();
+  localStorage.removeItem("list");
+}
+
+// delete item
+
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+
+  list.removeChild(element);
+
+  if (list.children.length === 0) {
+    container.classList.remove("show-container");
+  }
+  displayAlert("item removed", "danger");
+
+  setBackToDefault();
+  // remove from local storage
+  removeFromLocalStorage(id);
+}
+// edit item
+function editItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  // set edit item
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  // set form value
+  todo.value = editElement.innerHTML;
+  editFlag = true;
+  editID = element.dataset.id;
+  //
+  submitBtn.innerHTML = `<i class="fas fa-pen"></i>`;
+}
+// set backt to defaults
+function setBackToDefault() {
+  todo.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.innerHTML = `<i class="fas fa-plus"></i>`;
+}
+
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+  const todo = { id, value };
+  let items = getLocalStorage();
+  items.push(todo);
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+
+  items = items.filter((item) => {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+  let items = getLocalStorage();
+
+  items = items.map((item) => {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+// localStorage API
+// setItem
+// getItem
+// removeItem
+// save as string
+/*
+localStorage.setItem("Oracle", JSON.stringify(["Java", "Red Bull"]));
+const oracles = JSON.parse(localStorage.getItem("Oracle"));
+console.log(oracles);
+
+localStorage.removeItem("Oracle");
+*/
+
+// ****** SETUP ITEMS **********
+
+function setupItems() {
+  let items = getLocalStorage();
+
+  if (items.length > 0) {
+    items.forEach((item) => {
+      createListItem(item.id, item.value);
+    });
+    container.classList.add("show-container");
+  }
+}
+
+function createListItem(id, value) {
+  const element = document.createElement("article");
+  let attr = document.createAttribute("data-id");
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.classList.add("todo-item");
+  element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-pen"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-times"></i>
+              </button>
+            </div>
+          `;
+  // add event listeners to both buttons;
+  const deleteBtn = element.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", deleteItem);
+  const editBtn = element.querySelector(".edit-btn");
+  editBtn.addEventListener("click", editItem);
+
+  // append child
+  list.appendChild(element);
+}
